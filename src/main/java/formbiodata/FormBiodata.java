3bd1b7431ccaa66ff47c6c82ca8e04e4232fa61b
veir <veir@165-100-0-197.secomtrust.net>
2025-12-15 07:54:50 +0700

    initial commit

---
+++ src/main/java/formbiodata/FormBiodata.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package formbiodata;
+
+/**
+ *
+ * @author veir
+ */
+public class FormBiodata {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
